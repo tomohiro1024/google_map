70490cc8fc2d2dd56673c9371d549aea73ec657f
tomohiro1024 <gongyezhihong732@gmail.com>
2025-01-09 15:20:29 +0900

    UIの骨組みの作成

--- lib/search_page.dart
+++ lib/search_page.dart
@@ -10,10 +10,40 @@ class SearchPage extends StatefulWidget {
 class _MyWidgetState extends State<SearchPage> {
   @override
   Widget build(BuildContext context) {
+    double height = MediaQuery.of(context).size.height;
+    double width = MediaQuery.of(context).size.width;
     return Scaffold(
       appBar: AppBar(
         title: Center(child: Text('検索')),
         backgroundColor: Colors.blue,
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            ClipRRect(
+              borderRadius: BorderRadius.circular(20),
+              child: Image.network(
+                'https://lh5.googleusercontent.com/p/AF1QipOWtxNfoxL1rAuAsALYMRPJQEYWJ5JEn1sUxpkP=w408-h544-k-no',
+                width: width,
+                height: 300,
+                fit: BoxFit.cover,
+              ),
+            ),
+            SizedBox(height: height * 0.03),
+            Text(
+              '検索名',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 20,
+              ),
+            ),
+            SizedBox(height: height * 0.03),
+            ElevatedButton(
+              onPressed: () {},
+              child: Text('ボタン'),
+            ),
+          ],
+        ),
       ),
     );
   }
